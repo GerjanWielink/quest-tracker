bc0eed55a4cb44e75f87b92fe2936c6acb2be7df
Gerjan Wielink <gerjan.wielink@nedap.com>
2018-11-21 14:01:54 +0100

    Add warning

--- src/App.java
+++ src/App.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 
 public class App {
 	public static void main (String[] args) throws IOException {
+		if (args.length == 0) {
+			System.out.println("Username not provided");
+			return;
+		}
+
 		PlayerType playerType = args.length > 1 && args[1].equals("-i") ? PlayerType.IRONMAN : PlayerType.NORMAL;
 
 		System.out.print(HighScoreConnector.getPlayerStats(args[0], playerType));
